803dbdd8b8f647c7941289d7b9dcdf11bba895b6
Tom Rees-Herdman <tom@techanvil.co.uk>
2020-04-11 00:27:53 +0100

    remove menu etc

--- app/client/src/js/setupSocket.js
+++ app/client/src/js/setupSocket.js
@@ -9,11 +9,11 @@ var loggedData = false
 var sessionLog, sessionFooter, logDate, currentDate, myFile, errorExists
 var socket, termid // eslint-disable-line
 // DOM properties
-var status = document.getElementById('status')
-var header = document.getElementById('header')
-var dropupContent = document.getElementById('dropupContent')
-var footer = document.getElementById('footer')
-var countdown = document.getElementById('countdown')
+// var status = document.getElementById('status')
+// var header = document.getElementById('header')
+// var dropupContent = document.getElementById('dropupContent')
+// var footer = document.getElementById('footer')
+// var countdown = document.getElementById('countdown')
 
 export function setupSocket(term) {
   window.addEventListener('resize', resizeScreen, false)
@@ -54,73 +54,73 @@ export function setupSocket(term) {
 
   //socket.on('menu', function (data) { })
 
-  socket.on('status', function (data) {
-    status.innerHTML = data
-  })
+  // socket.on('status', function (data) {
+  //   status.innerHTML = data
+  // })
 
-  socket.on('ssherror', function (data) {
-    status.innerHTML = data
-    status.style.backgroundColor = 'red'
-    errorExists = true
-  })
+  // socket.on('ssherror', function (data) {
+  //   status.innerHTML = data
+  //   status.style.backgroundColor = 'red'
+  //   errorExists = true
+  // })
 
-  socket.on('headerBackground', function (data) {
-    header.style.backgroundColor = data
-  })
+  // socket.on('headerBackground', function (data) {
+  //   header.style.backgroundColor = data
+  // })
 
-  socket.on('header', function (data) {
-    if (data) {
-      header.innerHTML = data
-      header.style.display = 'block'
-      // header is 19px and footer is 19px, recaculate new terminal-container and resize
-      // FIXME terminalContainer.style.height = 'calc(100% - 38px)'
-      resizeScreen()
-    }
-  })
+  // socket.on('header', function (data) {
+  //   if (data) {
+  //     header.innerHTML = data
+  //     header.style.display = 'block'
+  //     // header is 19px and footer is 19px, recaculate new terminal-container and resize
+  //     // FIXME terminalContainer.style.height = 'calc(100% - 38px)'
+  //     resizeScreen()
+  //   }
+  // })
 
-  socket.on('footer', function (data) {
-    sessionFooter = data
-    footer.innerHTML = data
-  })
+  // socket.on('footer', function (data) {
+  //   sessionFooter = data
+  //   footer.innerHTML = data
+  // })
 
-  socket.on('statusBackground', function (data) {
-    status.style.backgroundColor = data
-  })
+  // socket.on('statusBackground', function (data) {
+  //   status.style.backgroundColor = data
+  // })
 
   //socket.on('allowreplay', function (data) { })
 
   //socket.on('allowreauth', function (data) { })
 
   socket.on('disconnect', function (err) {
-    if (!errorExists) {
-      status.style.backgroundColor = 'red'
-      status.innerHTML =
-        'WEBSOCKET SERVER DISCONNECTED: ' + err
-    }
+    // if (!errorExists) {
+    //   status.style.backgroundColor = 'red'
+    //   status.innerHTML =
+    //     'WEBSOCKET SERVER DISCONNECTED: ' + err
+    // }
     socket.io.reconnection(false)
-    countdown.classList.remove('active')
+    // countdown.classList.remove('active')
   })
 
-  socket.on('error', function (err) {
-    if (!errorExists) {
-      status.style.backgroundColor = 'red'
-      status.innerHTML = 'ERROR: ' + err
-    }
-  })
+  // socket.on('error', function (err) {
+  //   if (!errorExists) {
+  //     status.style.backgroundColor = 'red'
+  //     status.innerHTML = 'ERROR: ' + err
+  //   }
+  // })
 
   //socket.on('reauth', function () { })
 
   // safe shutdown
-  var hasCountdownStarted = false
+  // var hasCountdownStarted = false
 
-  socket.on('shutdownCountdownUpdate', function (remainingSeconds) {
-    if (!hasCountdownStarted) {
-      countdown.classList.add('active')
-      hasCountdownStarted = true
-    }
+  // socket.on('shutdownCountdownUpdate', function (remainingSeconds) {
+  //   if (!hasCountdownStarted) {
+  //     countdown.classList.add('active')
+  //     hasCountdownStarted = true
+  //   }
 
-    countdown.innerText = 'Shutting down in ' + remainingSeconds + 's'
-  })
+  //   countdown.innerText = 'Shutting down in ' + remainingSeconds + 's'
+  // })
 
   term.onTitleChange(function (title) {
     document.title = title
