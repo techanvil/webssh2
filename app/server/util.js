8feff9224d0a17424e25d43292e79b22b212b19a
Tom Rees-Herdman <tom@techanvil.co.uk>
2020-04-10 23:02:12 +0100

    remove basic auth

--- app/server/util.js
+++ app/server/util.js
@@ -16,18 +16,18 @@ exports.setDefaultCredentials = function (username, password, privatekey) {
 }
 
 exports.basicAuth = function basicAuth (req, res, next) {
-  var myAuth = Auth(req)
-  if (myAuth && myAuth.pass !== '') {
-    req.session.username = myAuth.name
-    req.session.userpassword = myAuth.pass
-    debug('myAuth.name: ' + myAuth.name.yellow.bold.underline +
-      ' and password ' + ((myAuth.pass) ? 'exists'.yellow.bold.underline
-      : 'is blank'.underline.red.bold))
-  } else {
-    req.session.username = defaultCredentials.username
-    req.session.userpassword = defaultCredentials.password
-    req.session.privatekey = defaultCredentials.privatekey
-  }
+  // var myAuth = Auth(req)
+  // if (myAuth && myAuth.pass !== '') {
+  //   req.session.username = myAuth.name
+  //   req.session.userpassword = myAuth.pass
+  //   debug('myAuth.name: ' + myAuth.name.yellow.bold.underline +
+  //     ' and password ' + ((myAuth.pass) ? 'exists'.yellow.bold.underline
+  //     : 'is blank'.underline.red.bold))
+  // } else {
+  req.session.username = defaultCredentials.username
+  req.session.userpassword = defaultCredentials.password
+  req.session.privatekey = defaultCredentials.privatekey
+  //}
   if ((!req.session.userpassword) && (!req.session.privatekey)) {
     res.statusCode = 401
     debug('basicAuth credential request (401)')
